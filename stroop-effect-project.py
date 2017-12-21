209d759d245e146129adf82469ea6deb8a23da3e
John Coletta <johnbcoletta@gmail.com>
2017-12-21 14:08:00 -0500

    Plotted the congruent data

--- stroop-effect-project.py
+++ stroop-effect-project.py
@@ -6,3 +6,13 @@ import scipy.stats as stats
 
 #Read in data
 df = pd.read_csv('stroopdata.csv')
+
+#Build some visualizations here
+plt.scatter(range(1, df.shape[0] + 1), df['Congruent'])
+plt.axhline(df['Congruent'].mean(), color='r')
+plt.xlabel('Task Number')
+plt.ylabel('Time (s)')
+plt.title('Time to Complete Congruent Test')
+plt.savefig('congruentplot.png')
+plt.show()
+plt.clf()
